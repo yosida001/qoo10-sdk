0f79c3bf209a2f78888d48aae5d83492cdfbf98d
tanabe <tanabe@appeal-w.co.jp>
2026-05-22 12:45:18 +0900

    [fix]EditGoodsOptionRequestのAdditionalOptionのパラメータ名を誤っていたため修正
    パラメータ名が違うことにより、オプションが全消しされる

--- src/Requests/ItemsOptions/EditGoodsOptionRequest.php
+++ src/Requests/ItemsOptions/EditGoodsOptionRequest.php
@@ -24,7 +24,7 @@ class EditGoodsOptionRequest extends AbstractRequest
         return [
             'ItemCode',
             'SellerCode',
-            'AdditionalOptions',
+            'AdditionalOption',
         ];
     }
 }
